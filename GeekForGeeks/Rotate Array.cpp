f5c0c6675eff2584578b3ba0ef9430ec224baae3
Kautilya DK <kautilyadk@gmail.com>
2024-11-12 23:50:20 +0530

    Rotate Array(GFG)

---
+++ GeekForGeeks/Rotate Array.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    void rotateArr(vector<int>& arr, int d) {
+        // code here
+        d = d % arr.size();
+        
+        vector<int> temp;
+        for(int i = 0 ; i < d ; i++)
+            temp.push_back(arr[i]);
+        
+        for(int i = 0 ; i + d < arr.size() ; i++){
+            arr[i] = arr[i+d];
+        }
+        
+        for(int i = arr.size() - d ; i < arr.size() ; i++)
+            arr[i] = temp[i - (arr.size() - d)];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int d;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        d = crr[0];
+        int n = arr.size();
+        Solution ob;
+        // calling rotateArr() function
+        ob.rotateArr(arr, d);
+
+        // printing the elements of the array
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
