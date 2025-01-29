e1f0c5b05d25b4585cdd018f5b59673a11273740
Kautilya DK <kautilyadk@gmail.com>
2025-01-29 23:24:58 +0530

    Binary Tree Inorder Traversal (Iterative Method) (L-94)

--- Leetcode/Binary Tree InOrder Traversal(L-94).cpp
+++ Leetcode/Binary Tree InOrder Traversal(L-94).cpp
@@ -10,18 +10,34 @@
  * };
  */
 class Solution {
-private:
-    void inorder(TreeNode *root , vector<int> &arr){
-        if(root != NULL){
-            inorder(root->left , arr);
-            arr.push_back(root->val);
-            inorder(root->right , arr);
-        }
-    }
+// private:
+//     void inorder(TreeNode *root , vector<int> &arr){
+//         if(root != NULL){
+//             inorder(root->left , arr);
+//             arr.push_back(root->val);
+//             inorder(root->right , arr);
+//         }
+//     }
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> arr;
-        inorder(root , arr);
+        // inorder(root , arr);
+        stack<TreeNode*> st;
+
+        while(true){
+            if(root != NULL){
+                st.push(root);
+                root = root->left;
+            }
+
+            else{
+                if(st.empty()) break;
+                root = st.top();
+                st.pop();
+                arr.push_back(root->val);
+                root = root->right;
+            }
+        }
         return arr;
     }
 };
