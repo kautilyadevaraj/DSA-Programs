299bf3fb9134352ab4634d0b188deaa97b3b2c61
Kautilya DK <kautilyadk@gmail.com>
2025-02-16 18:42:16 +0530

    Tree Traversals(CN)

--- CodeNinjas/Tree Traversals.cpp
+++ CodeNinjas/Tree Traversals.cpp
@@ -11,45 +11,56 @@
         TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
     };
 */
-void inorderTraversal(TreeNode *root , vector<int> &arr){
-    if(root != NULL){
-        inorderTraversal(root->left , arr);
-        arr.push_back(root->data);
-        inorderTraversal(root->right , arr);
-    }
-}
-
-void preorderTraversal(TreeNode *root , vector<int> &arr){
-    if(root != NULL){
-        arr.push_back(root->data);
-        preorderTraversal(root->left , arr);
-        preorderTraversal(root->right , arr);
-    }
-}
-
-void postorderTraversal(TreeNode *root , vector<int> &arr){
-    if(root != NULL){
-        postorderTraversal(root->left , arr);
-        postorderTraversal(root->right , arr);
-        arr.push_back(root->data);
-    }
-}
-
+/*
+    Following is Binary Tree Node structure:
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+*/
 vector<vector<int>> getTreeTraversal(TreeNode *root){
     // Write your code here.
-    vector<int> inorder;
-    inorderTraversal(root , inorder);
-
-    vector<int> preorder;
-    preorderTraversal(root , preorder);
-
-    vector<int> postorder;
-    postorderTraversal(root , postorder);
-
     vector<vector<int>> ans;
-    ans.push_back(inorder);
-    ans.push_back(preorder);
-    ans.push_back(postorder);
-    return ans;
+    stack<pair<TreeNode*, int>> st;
+    st.push({root , 1});
+    vector<int> pre, post, inorder;
 
+    if(root == NULL) return ans;
+
+    while(!st.empty()){
+        auto it = st.top();
+        st.pop();
+
+        if(it.second == 1){
+            pre.push_back(it.first->data);
+            it.second++;
+            st.push(it);
+
+            if(it.first->left != NULL)
+                st.push({it.first->left , 1});
+        }
+
+        else if(it.second == 2){
+            inorder.push_back(it.first->data);
+            it.second++;
+            st.push(it);
+
+            if(it.first->right != NULL)
+                st.push({it.first->right , 1});
+        }
+
+        else{
+            post.push_back(it.first->val);
+        }
+    }
+    ans.push_back(inorder);
+    ans.push_back(pre);
+    ans.push_back(post);
+
+    return ans;
 }
