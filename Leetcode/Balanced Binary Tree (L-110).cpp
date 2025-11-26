d4e06a4523c0415d99fd5de767aefc239a08d187
kautilyadevaraj <kautilyadk@gmail.com>
2025-11-26 21:16:58 +0530

    Balanced Binary Tree (L-110)

---
+++ Leetcode/Balanced Binary Tree (L-110).cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int findHeight(TreeNode* root){
+        if(root == NULL)
+            return 0;
+
+        int lh = findHeight(root->left);
+
+        if(lh == -1)
+            return -1;
+        
+        int rh = findHeight(root->right);
+
+        if(rh == -1)
+            return -1;
+        if(abs(lh - rh) > 1) return -1;
+        return max(findHeight(root->left) , findHeight(root->right)) + 1;
+    }
+public:
+    bool isBalanced(TreeNode* root) {
+        return findHeight(root) != -1;
+    }
+};
