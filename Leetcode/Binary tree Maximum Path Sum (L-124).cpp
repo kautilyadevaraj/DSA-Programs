e6d1df60dacf4a43c2e95fbfa50ca90a648adf1d
kautilyadevaraj <kautilyadk@gmail.com>
2025-11-26 21:14:46 +0530

    Binary Tree Maximum Path Sum (L-124)

---
+++ Leetcode/Binary tree Maximum Path Sum (L-124).cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int maxi = INT_MIN;
+    int findMaxPath(TreeNode* root){
+        if(root == NULL)
+            return 0;
+
+        int lh = max(0 , findMaxPath(root->left));
+        int rh = max(0 , findMaxPath(root->right));
+
+        maxi = max(maxi , lh + rh + root->val);
+
+        return root->val + max(lh , rh);
+    }
+public:
+    int maxPathSum(TreeNode* root) {
+        findMaxPath(root);
+
+        return maxi;
+    }
+};
