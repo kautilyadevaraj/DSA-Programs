a325fe3625ecc17b2c9af89658407e91f61a837a
kautilyadevaraj <kautilyadk@gmail.com>
2026-01-14 11:51:45 +0530

    Implementing Dijkstra Algorithm

--- GeekForGeeks/Implementing Dijkstra's Algorithm.cpp
+++ GeekForGeeks/Implementing Dijkstra's Algorithm.cpp
@@ -1,3 +1,5 @@
+
+//using priority queue
 class Solution {
     public:
       vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
@@ -35,3 +37,40 @@ class Solution {
           return result;
       }
   };
+
+  //using set
+  vector<vector<pair<int,int>>> adj(V);
+        
+        for(auto& e: edges){
+            adj[e[0]].push_back({e[1],e[2]});
+            adj[e[1]].push_back({e[0],e[2]});
+        }
+        
+        vector<int> result(V,INT_MAX);
+        set<pair<int,int>> st;
+        st.insert({0,src});
+        result[src] = 0;
+        
+        while(!st.empty()){
+            auto& it = *st.begin();
+            int node = it.second;
+            int dist = it.first;
+            st.erase(it);
+            
+            for(auto& v : adj[node]){
+                int adjNode = v.first;
+                int weight = v.second;
+                int newDist = dist + weight;
+                
+                if(newDist < result[adjNode]){
+                    
+                    if(result[adjNode] != INT_MAX)
+                        st.erase({result[adjNode],adjNode});
+                        
+                    result[adjNode] = newDist;
+                    st.insert({newDist,adjNode});
+                }
+            }
+        }
+        
+        return result;
