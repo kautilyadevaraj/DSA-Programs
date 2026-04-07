a683bf16b5c9d0b268cee5accc8333620cc83553
kautilyadevaraj <kautilyadk@gmail.com>
2026-04-07 20:29:16 +0530

    print-longest-common-subsequence (CN)

--- CodeNinjas/Print Longest Common Subsequences.cpp
+++ CodeNinjas/Print Longest Common Subsequences.cpp
@@ -1,22 +1,35 @@
 //https://www.naukri.com/code360/problems/print-longest-common-subsequence_8416383?leftPanelTabValue=PROBLEM
 
 string findLCS(int n, int m,string &text1, string &text2){
-    vector<vector<string>> dp(n + 1 , vector<string>(m + 1 , ""));
+    vector<vector<int>> dp(n + 1 , vector<int>(m + 1 , 0));
 
     for(int i = 1 ; i < n + 1 ; i++){
         for(int  j = 1 ; j < m + 1 ; j++){
             if(text1[i - 1] == text2[j - 1])
-                dp[i][j] = text1[i - 1] + dp[i - 1][j - 1];
+                dp[i][j] = 1 + dp[i - 1][j - 1];
 
-            else{
-                if(dp[i][j - 1].size() >= dp[i - 1][j].size())
-                    dp[i][j] = dp[i][j - 1];
-                else
-                    dp[i][j] = dp[i - 1][j];
-            }
+            else
+                dp[i][j] = max(dp[i][j - 1] , dp[i - 1][j]);
         }
     }
-    string ans = dp[n][m];
+
+    int i = n, j = m;
+    string ans = "";
+
+    while(i > 0 && j > 0){
+        if(text1[i - 1] == text2[j - 1]){
+            ans += text1[i - 1];
+            i--;
+            j--;
+        }
+
+        else if(dp[i][j - 1] > dp[i - 1][j])
+            j--;
+        else    
+            i--;
+    }
+
     reverse(ans.begin() , ans.end());
-    return ans;	
+
+    return ans;
 }
